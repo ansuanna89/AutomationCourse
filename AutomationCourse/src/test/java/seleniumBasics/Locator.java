2d47ac2c3b9d8a0644deed77940d3c13a0832ada
mathe <mathe@10.0.0.34>
2025-09-16 17:29:17 +0200

    WebElementCommands

--- AutomationCourse/src/test/java/seleniumBasics/Locator.java
+++ AutomationCourse/src/test/java/seleniumBasics/Locator.java
@@ -16,6 +16,32 @@ public class Locator extends Base{
 			driver.findElement(By.cssSelector("button[id='button-one']"));
 			//xpath
 			
+			/*Absolute XPath
+			/html/body/section/div/div/div[2]/div[1]/div/div[2]/form/button
+			
+			 Relative XPath
+			 
+			 //button[@id='button-one']
+			 //button[text()='Show Message']
+			 //button[starts-with(text(),'Show ')]
+			 //button[@type='button' and @id='button-one']
+			 //button[@id='button-one' or @id='button-one-electronics']
+			 //div[contains (text(), 'Single Input Field')]//parent::div[@class='card']	- immediately above tag
+			 //div[@class='card']//child::button[@id='button-one']- immediately below
+			 //button[@id='button-one']//following::div[@class='card'] --> consider all the following elements containing the attribute class = card
+			 //button[@id='button-one']//preceding:: div[@class='card'] -->consider all the elements above button-one
+			 
+			 ancestor---considering one parent, above another parent , above another parent, ie parent, grand parent, grate grand parent
+			//button[@id='button-one']//ancestor::div
+			//div[@class='card']//descendant::div --->Child div, grand child div, great grand child div 
+			 
+			 */
+			
+			driver.findElement(By.xpath("//button[@id='button-one']"));
+			driver.findElement(By.xpath("//button[text()='Show Message']"));
+			driver.findElement(By.xpath("//button[starts-with(text(),'Show ')]"));
+			
+			
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
---
+++ AutomationCourse/src/test/java/seleniumBasics/WebElementCommands.java
@@ -0,0 +1,29 @@
+package seleniumBasics;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class WebElementCommands extends Base{
+	
+	public void verifyWebElementCommands() {
+		
+		driver.navigate().to("https://selenium.qabible.in/simple-form-demo.php");
+		WebElement messageBox = driver.findElement(By.xpath("//input[@id='single-input-field']"));
+		messageBox.sendKeys("Hello Eve");
+		WebElement showMessageBtn = driver.findElement(By.id("button-one"));
+		showMessageBtn.click();
+		WebElement yourMsgTxt = driver.findElement(By.id("message-one"));
+		System.out.println(yourMsgTxt.getText());
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		WebElementCommands commands = new WebElementCommands();
+		commands.initializeBrowser();
+		commands.verifyWebElementCommands();
+
+	}
+
+}
