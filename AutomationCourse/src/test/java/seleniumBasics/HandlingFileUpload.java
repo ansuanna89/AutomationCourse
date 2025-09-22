64155915d6de08f2a1594285cd075f971bbfc929
mathe <mathe@10.0.0.34>
2025-09-22 17:39:34 +0200

    File Upload, Window Handling

---
+++ AutomationCourse/src/test/java/seleniumBasics/HandlingFileUpload.java
@@ -0,0 +1,67 @@
+package seleniumBasics;
+
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.datatransfer.StringSelection;
+import java.awt.event.KeyEvent;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class HandlingFileUpload extends Base{
+
+	public void verifyFileUploadUsingSendKeys() {
+		
+		driver.navigate().to("https://demo.guru99.com/test/upload/");
+		WebElement fileUploadBtn = driver.findElement(By.id("uploadfile_0"));
+		fileUploadBtn.sendKeys("C:\\Users\\mathe\\git\\AutomationCourse\\AutomationCourse\\src\\test\\resources\\Abstraction.pdf");
+		
+		WebElement checkBoxTerms = driver.findElement(By.id("terms"));
+		checkBoxTerms.click();
+		
+		WebElement submitBtn = driver.findElement(By.id("submitbutton"));
+		submitBtn.click();
+		
+	}
+	
+	public void fileUploadUsingRobot() throws AWTException {
+		
+		driver.navigate().to("https://www.ilovepdf.com/pdf_to_word");
+		WebElement pickFileBtn = driver.findElement(By.id("pickfiles"));
+		pickFileBtn.click();
+		
+		StringSelection stringSelection = new StringSelection("C:\\Users\\mathe\\git\\AutomationCourse\\AutomationCourse\\src\\test\\resources\\Abstraction.pdf");
+		Toolkit.getDefaultToolkit().getSystemClipboard().setContents(stringSelection, null);
+		
+		Robot r=new Robot();
+		r.delay(2500);
+		r.keyPress(KeyEvent.VK_ENTER);
+		r.keyRelease(KeyEvent.VK_ENTER);
+		r.keyPress(KeyEvent.VK_CONTROL);
+		r.keyPress(KeyEvent.VK_V);
+		r.keyRelease(KeyEvent.VK_CONTROL);
+		r.keyRelease(KeyEvent.VK_V);
+		r.keyPress(KeyEvent.VK_ENTER);
+		r.keyRelease(KeyEvent.VK_ENTER);
+		
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		HandlingFileUpload handlingFileUpload = new HandlingFileUpload();
+		handlingFileUpload.initializeBrowser();
+		//handlingFileUpload.verifyFileUploadUsingSendKeys();
+		try {
+			handlingFileUpload.fileUploadUsingRobot();
+		} catch (AWTException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+}
