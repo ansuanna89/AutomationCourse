b3a80ad7cb2642c5320aeaafa8a1b82e95af45fc
mathe <mathe@10.0.0.34>
2025-09-30 17:25:05 +0200

    Explicit Wait and Fluent Wait

--- AutomationCourse/src/test/java/seleniumBasics/WebElementCommands.java
+++ AutomationCourse/src/test/java/seleniumBasics/WebElementCommands.java
@@ -1,7 +1,16 @@
 package seleniumBasics;
 
+import java.time.Duration;
+import java.util.NoSuchElementException;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class WebElementCommands extends Base{
 	
@@ -14,6 +23,13 @@ public class WebElementCommands extends Base{
 		WebElement showMessageBtn = driver.findElement(By.id("button-one"));
 		System.out.println(showMessageBtn.isDisplayed());
 		System.out.println(showMessageBtn.isEnabled());
+		
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+		wait.until(ExpectedConditions.elementToBeClickable(showMessageBtn));
+		
+		Wait<WebDriver> fluentWait = new FluentWait<WebDriver>(driver).withTimeout(Duration.ofSeconds(5)).pollingEvery(Duration.ofSeconds(1)).ignoring(NoSuchElementException.class);
+		fluentWait.until(ExpectedConditions.elementToBeClickable(showMessageBtn));
+		
 		showMessageBtn.click();
 		WebElement yourMsgTxt = driver.findElement(By.id("message-one"));
 		System.out.println(yourMsgTxt.getText());
